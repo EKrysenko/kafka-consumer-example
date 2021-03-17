3f820b166c47c364b74b43d47048dcb5eacf8a30
evgenii_krysenko <evgenii_krysenko@epam.com>
2021-03-17 13:45:05 +0300

    Consumer config with correct key/message types

--- src/main/java/com/elcompanies/eventhubkafkaconsumer/config/KafkaConsumerConfig.java
+++ src/main/java/com/elcompanies/eventhubkafkaconsumer/config/KafkaConsumerConfig.java
@@ -1,8 +1,8 @@
 package com.elcompanies.eventhubkafkaconsumer.config;
 
+import com.elcompanies.eventhubkafkaconsumer.model.ConsumerMessage;
 import com.google.common.primitives.Ints;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -13,29 +13,27 @@ import org.springframework.kafka.listener.adapter.RecordFilterStrategy;
 
 import java.util.Arrays;
 
-import static java.util.Base64.getEncoder;
-
 @Configuration
 @EnableKafka
 public class KafkaConsumerConfig {
 
     @Bean
     @ConditionalOnMissingBean(name = "kafkaListenerContainerFactory")
-    public ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
-            ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
-            ConsumerFactory<Object, Object> kafkaConsumerFactory) {
-        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+    public ConcurrentKafkaListenerContainerFactory<String, ConsumerMessage> kafkaListenerContainerFactory(
+            ConsumerFactory<String, ConsumerMessage> kafkaConsumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<String, ConsumerMessage> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
+        factory.setConsumerFactory(kafkaConsumerFactory);
         // filter messages by custom header
-        factory.setRecordFilterStrategy(recordFilterStrategy());
-        configurer.configure(factory, kafkaConsumerFactory);
+        factory.setRecordFilterStrategy(record -> 13 != record.value().getProcessAtMinutes());
+//        factory.setRecordFilterStrategy(recordFilterStrategy());
         return factory;
     }
 
-    public RecordFilterStrategy recordFilterStrategy() {
-        return (RecordFilterStrategy) consumerRecord -> Arrays.stream(consumerRecord.headers().toArray())
+    public RecordFilterStrategy<String, ConsumerMessage> recordFilterStrategy() {
+        return consumerRecord -> Arrays.stream(consumerRecord.headers().toArray())
                 .anyMatch(header ->
                         "filter".equals(header.key())
-                                && Ints.fromByteArray(header.value())!=14);
+                                && Ints.fromByteArray(header.value()) != 14);
     }
 }
